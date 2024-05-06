786c484bd1199323f7f5ee9bd411da6a7a1b4e95
Yuchan Jeon <eukkchan03@gmail.com>
2024-05-06 15:34:34 +0900

    Update LlamaBot.py

--- LlamaBot.py
+++ LlamaBot.py
@@ -27,8 +27,12 @@ async def on_ready():
 # 도움말 명령어
 @bot.command()
 async def 도움말(ctx):
-    embed=discord.Embed(title="본국 제1 본정보부 첩보부서", url="https://github.com/eukkcha/LlamaBot", description="조선인민국 호위총국 제14과 첨단양자전산화공수특전대 산하 주식회사 에버랜드 소속 전자동화어트랙션지원병력.:llama:\n\n해당 전동화병기는 시범 운영 중에 있음.\n\n본국 제1 본정보부 첩보부서에서 그동안 작전해온 기밀들을 낱낱히 공개하도록 하겠음.", color=0xf43d8f)
-    embed.set_author(name="라마봇", url="https://github.com/eukkcha/LlamaBot")
+    embed=discord.Embed(title="본국 제1 본정보부 첩보부서", 
+                        url="https://github.com/eukkcha/LlamaBot", 
+                        description="조선인민국 호위총국 제14과 첨단양자전산화공수특전대 산하 주식회사 에버랜드 소속 전자동화어트랙션지원병력.:llama:\n\n해당 전동화병기는 시범 운영 중에 있음.\n\n본국 제1 본정보부 첩보부서에서 그동안 작전해온 기밀들을 낱낱히 공개하도록 하겠음.", 
+                        color=0xf43d8f)
+    embed.set_author(name="라마봇", 
+                     url="https://github.com/eukkcha/LlamaBot")
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/1233535085635043390/3a12cd88c71c6c8c6fabb0ef26fb07ca.png?size=2048")
     
     embed.add_field(name="`라마 도움말`", value="도움말 출력", inline=True)
